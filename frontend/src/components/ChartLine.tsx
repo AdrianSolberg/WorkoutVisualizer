3d7009365032ae3d14d2d993cffd5e5d1ef16dca
Adrian Solberg <adrianhsolberg@gmail.com>
2025-08-11 20:30:25 +0200

    update: remove unused toggle

--- frontend/src/components/ChartLine.tsx
+++ frontend/src/components/ChartLine.tsx
@@ -15,7 +15,6 @@ import {
     ChartTooltipContent,
 } from "@/shadcn/ui/chart";
 export const description = "An interactive line chart";
-import { useState } from "react";
 import { ToggleGroup, ToggleGroupItem } from "@/shadcn/ui/toggle-group";
 import {
     Select,
@@ -47,8 +46,6 @@ export function ChartLine({
     timeRange,
     setTimeRange,
 }: ChartLineInterface) {
-    const [activeChart, setActiveChart] = useState<string>("morning_weight");
-
     return (
         <Card className="py-4 sm:py-0">
             <CardHeader className="flex flex-col items-stretch border-b !p-0 sm:flex-row">
@@ -58,57 +55,6 @@ export function ChartLine({
                         Showing morning weight over time
                     </CardDescription>
                 </div>
-                <CardAction className="pb-3 sm:pb-0 pt-4">
-                    <ToggleGroup
-                        type="single"
-                        value={activeChart}
-                        onValueChange={setActiveChart}
-                        variant="outline"
-                        className="hidden *:data-[slot=toggle-group-item]:!px-4 @[767px]/card:flex"
-                    >
-                        <ToggleGroupItem value="90d">
-                            Morning weight
-                        </ToggleGroupItem>
-                        <ToggleGroupItem value="90d">
-                            Incline Dumbell Press
-                        </ToggleGroupItem>
-                        <ToggleGroupItem value="year">
-                            Military press
-                        </ToggleGroupItem>
-                        <ToggleGroupItem value="all_time">
-                            Pullups
-                        </ToggleGroupItem>
-                    </ToggleGroup>
-                    <Select value={activeChart} onValueChange={setActiveChart}>
-                        <SelectTrigger
-                            className="flex w-40 **:data-[slot=select-value]:block **:data-[slot=select-value]:truncate @[767px]/card:hidden"
-                            size="sm"
-                            aria-label="Select a value"
-                        >
-                            <SelectValue placeholder="Morning weight" />
-                        </SelectTrigger>
-                        <SelectContent className="rounded-xl">
-                            <SelectItem
-                                value="morning_weight"
-                                className="rounded-lg"
-                            >
-                                Morning weight
-                            </SelectItem>
-                            <SelectItem value="skråbenk" className="rounded-lg">
-                                Incline dumbell press
-                            </SelectItem>
-                            <SelectItem
-                                value="militærpress"
-                                className="rounded-lg"
-                            >
-                                Military press
-                            </SelectItem>
-                            <SelectItem value="pullups" className="rounded-lg">
-                                Pullups
-                            </SelectItem>
-                        </SelectContent>
-                    </Select>
-                </CardAction>
                 <CardAction className="px-6 pb-3 sm:pb-0 pt-4">
                     <ToggleGroup
                         type="single"
